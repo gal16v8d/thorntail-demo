f2423a7fb9a13d89e03bfc9391cdf449f78db6b5
Alex Galvis <alex.galvis.sistemas@gmail.com>
2020-04-16 19:22:06 -0500

    Init repo

---
+++ README.md
@@ -0,0 +1,7 @@
+# Thorntail DEMO
+
+Run using the command -> mvn thorntail:run
+Once ready you can check this endpoints:
+
+* http://localhost:8080/api/hello 
+* http://localhost:8080/api/resource 
---
+++ src/main/java/co/com/gsdd/rest/RestApplication.java
@@ -0,0 +1,8 @@
+package co.com.gsdd.rest;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("/api")
+public class RestApplication extends Application {
+}
